09088ecc8631f5fd0714f652efa5e71deed77704
Kris Govertsen <77295947+govertsenk@users.noreply.github.com>
2022-08-22 11:23:20 -0400

    modfied heat wave analysis for worcester

---
+++ examples/run_worcester_study.bat
@@ -0,0 +1,57 @@
+START /B python -m worcester_heat_wave_analysis --start_year=2020 --scenario=SSP5-8.5 --random_seed=186345385
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2020 --scenario=SSP3-7.0 --random_seed=861262795
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2020 --scenario=SSP2-4.5 --random_seed=881849381
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2020 --scenario=SSP1-2.6 --random_seed=216817523
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2020 --scenario=SSP1-1.9 --random_seed=816178610
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2025 --scenario=SSP5-8.5 --random_seed=519419765
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2025 --scenario=SSP3-7.0 --random_seed=481813753
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2025 --scenario=SSP2-4.5 --random_seed=808347862
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2025 --scenario=SSP1-2.6 --random_seed=318368167
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2025 --scenario=SSP1-1.9 --random_seed=781638320
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2030 --scenario=SSP5-8.5 --random_seed=413788303
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2030 --scenario=SSP3-7.0 --random_seed=587138489
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2030 --scenario=SSP2-4.5 --random_seed=631814736
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2030 --scenario=SSP1-2.6 --random_seed=818530853
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2030 --scenario=SSP1-1.9 --random_seed=908743810
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2035 --scenario=SSP5-8.5 --random_seed=738137851
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2035 --scenario=SSP3-7.0 --random_seed=381385411
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2035 --scenario=SSP2-4.5 --random_seed=114681316
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2035 --scenario=SSP1-2.6 --random_seed=891684310
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2035 --scenario=SSP1-1.9 --random_seed=313413857
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2040 --scenario=SSP5-8.5 --random_seed=735146441
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2040 --scenario=SSP3-7.0 --random_seed=451381343
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2040 --scenario=SSP2-4.5 --random_seed=355134851
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2040 --scenario=SSP1-2.6 --random_seed=374851313
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2040 --scenario=SSP1-1.9 --random_seed=163854733
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2045 --scenario=SSP5-8.5 --random_seed=813513111
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2045 --scenario=SSP3-7.0 --random_seed=111211211
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2045 --scenario=SSP2-4.5 --random_seed=748653548
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2045 --scenario=SSP1-2.6 --random_seed=661244811
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2045 --scenario=SSP1-1.9 --random_seed=228811461
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2050 --scenario=SSP5-8.5 --random_seed=813684351
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2050 --scenario=SSP3-7.0 --random_seed=543543515
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2050 --scenario=SSP2-4.5 --random_seed=214564841
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2050 --scenario=SSP1-2.6 --random_seed=744458463
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2050 --scenario=SSP1-1.9 --random_seed=888364544
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2055 --scenario=SSP5-8.5 --random_seed=341548644
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2055 --scenario=SSP3-7.0 --random_seed=845648513
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2055 --scenario=SSP2-4.5 --random_seed=811531513
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2055 --scenario=SSP1-2.6 --random_seed=213453515
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2055 --scenario=SSP1-1.9 --random_seed=132546431
+
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2060 --scenario=SSP5-8.5 --random_seed=543654351
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2060 --scenario=SSP3-7.0 --random_seed=135351031
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2060 --scenario=SSP2-4.5 --random_seed=151435151
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2060 --scenario=SSP1-2.6 --random_seed=935851351
+@REM START /B python -m worcester_heat_wave_analysis --start_year=2060 --scenario=SSP1-1.9 --random_seed=881325135
+
+
+
+
